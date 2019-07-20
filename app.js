99f85cdce3c72fef462ee0162b4b6d355eea8a30
imperia <egor@katagarov.ru>
2019-07-21 02:09:52 +0400

    add templates model

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const express = require('express');
-const path = require('path');
-const app = express();
+const routes = require('./routes');
+
+let app = express();
 const port = 3000;
 
-var knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host : '127.0.0.1',
-        user : 'root',
-        password : '1234',
-        database : 'gosuslugi'
-    }
-});
-
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// in module
-
-app.get('/', function(req, res, next) {
-    res.sendFile(path.resolve('route.html'));
-});
-
-app.get('/users/get/:id', function(req, res, next) {
-    let id = req.params.id;
-
-    if (id) {
-        knex('users')
-            .where({user_id: id})
-            .asCallback(function(err, row) {
-                res.json(row[0]);
-            });
-    }else{
-        res.json({err: 'Ничего не найдено!'});
-    }
-});
-
+app = routes.getRoutes(app);
 
 app.listen(port, function() {
     console.log('Start server');
---
+++ routes.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const users = require('./models/users');
+
+module.exports = {
+    getRoutes(app) {
+
+        // Users
+        app.get('/users/get/:id', users.get);
+        app.post('/users/add', users.add.bind(users));
+        app.post('/users/set/:id', users.set.bind(users));
+        app.get('/users/delete/:id', users.destroy);
+
+        // Templates
+        
+
+        // Common
+        app.get('/', function(req, res) {
+            return res.sendFile(path.resolve('route.html'));
+        });
+
+        return app;
+    }
+};
