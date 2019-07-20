7209ee7a727cdb8c13b89422806e07b9ac0ad54f
imperia <egor@katagarov.ru>
2019-07-21 02:12:09 +0400

    delete public folder

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const routes = require('./routes');
 let app = express();
 const port = 3000;
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ module.exports = {
         app.get('/users/delete/:id', users.destroy);
 
         // Templates
-        
+
 
         // Common
         app.get('/', function(req, res) {
